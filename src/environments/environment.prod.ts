f0a4bcf4d7501ca2161fba67970753fe2845e6fc
Raul Santos <raul.santos.external@atos.net>
2021-05-31 18:03:23 +0200

    prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+import { KeycloakOptions } from 'keycloak-angular';
+
+// Keycloak config
+const keycloakSetUp: KeycloakOptions = {
+  config: {
+    url: 'https://hidalgo-idm.hlrs.de/auth/',
+    realm: 'Hidalgo',
+    clientId: 'frontend',
+  },
+  initOptions: {
+    responseMode: 'fragment',
+    flow: 'standard',
+    onLoad: 'login-required',
+    checkLoginIframe: false
+  },
+  enableBearerInterceptor: true,
+};
+
 export const environment = {
-  production: true
+  production: true,
+  keycloak: keycloakSetUp,
+  apiUrl: 'http://sophora-105.man.poznan.pl/'
 };
